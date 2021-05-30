18ed1722d717ec2f780cb7282b7a117f3b222b9c
charmichles <charmichles@gmail.com>
2021-05-30 17:42:38 +0300

    added second best MST algorithm

--- graph-MST-py/LabAPCM.py
+++ graph-MST-py/LabAPCM.py
@@ -8,6 +8,7 @@ from __future__ import annotations
 import sys
 from Tree import Tree, TreeNode, TreeNodeInfo
 from WeightedGraph import WeightedGraph, WeightedGraphEdge
+from copy import deepcopy
 
 
 def grafDinamic(G : WeightedGraph, e : WeightedGraphEdge, algoritmAPCM):
@@ -60,5 +61,77 @@ def grafDinamic(G : WeightedGraph, e : WeightedGraphEdge, algoritmAPCM):
     fout.close()
 
 
+def secondBestMST(G : WeightedGraph, algoritmAPCM):
+    fout = open("SecondBestMST.out", 'w')
+
+    T = None
+    if algoritmAPCM == 'Kruskal':
+        T = G.KruskalMST()
+    elif algoritmAPCM == 'Prim':
+        T = G.PrimMST()
+    else:
+        raise Exception("Pentru secondBestMST parametrul algoritmAPCM trebuie sa fie egal cu 'Kruskal' sau 'Prim'\n")
+    cost_mst = sum([muchie.cost for muchie in T])
+
+    # fac o lista cu label-ul varfurilor din MST
+    mst_node_labels = set()
+    for edge in T:
+        mst_node_labels.add(edge.vertex1)
+        mst_node_labels.add(edge.vertex2)
+    # creez un dictionar care asociaza label-ul unui nod cu obiectul nod propriu-zis din Tree
+    mst_nodes = dict( [(label, TreeNode(TreeNodeInfo(sys.maxsize, label))) for label in mst_node_labels] )
+    mst = Tree(mst_nodes)
+    adj_set_mst = WeightedGraph.get_adjacency_set(T)
+
+    # am vazut la problema Graf Dinamic cum putem determina daca exista un APCM cu cost mai mic prin inchiderea unui ciclu si eliminarea muchiei maxime
+    # are sens atunci sa pot obtine si un APCM cu cost mai mare prin acelasi procedeu astfel:
+    # - pentru fiecare muchie care nu este in APCM o sa o adaug la APCM, astfel inchizand un ciclu
+    # - elimin muchia maxima din ciclul format(excluzand muchia tocmai adaugata) si calculez costul noului APCM in O(1)
+    # - compar aceste rezultate intre ele, iar minimul este al doilea cel mai bun APCM
+    adj_set_G = WeightedGraph.get_adjacency_set(G.edges)
+    edges_notinMST = []
+    aux_set = set()
+    # fac lista cu muchii care nu sunt in APCM
+    for vertex1, adj_vertex1 in adj_set_G.items():
+        for vertex2, cost in adj_vertex1.items():
+            if adj_set_mst.get(vertex1).get(vertex2) is None and (vertex1, vertex2) not in aux_set:
+                edges_notinMST.append(WeightedGraphEdge(vertex1, vertex2, cost))
+                aux_set.add((vertex1, vertex2))
+                aux_set.add((vertex2, vertex1))
+
+    edge_old, edge_new = None, None
+    cost_secondbestMST = None
+    for edge in edges_notinMST:
+        # fac muchia maxima(exceptand muchia adaugata) din ciclul inchis de muchia adaugata
+        mst_nodes = dict( [(label, TreeNode(TreeNodeInfo(sys.maxsize, label))) for label in mst_node_labels] )
+        mst.nodes = mst_nodes
+        mst.link_nodes(mst_nodes[edge.vertex1], adj_set_mst)
+
+        cycle_labels = mst.find(mst_nodes[edge.vertex2])
+        
+        cycle_edges = []
+        for i in range(len(cycle_labels) - 1):
+            cycle_edges.append(WeightedGraphEdge(cycle_labels[i], cycle_labels[i+1], adj_set_G[cycle_labels[i]][cycle_labels[i+1]]))
+
+        max_edge = max(cycle_edges, key = lambda edge : edge.cost)
+        # calculez costul noului APCM si compar
+        cost_new = cost_mst - max_edge.cost + edge.cost
+        if cost_secondbestMST is None or cost_new < cost_secondbestMST:
+            cost_secondbestMST = cost_new
+            edge_old = max_edge
+            edge_new = edge
+
+    fout.write(f'Primul\nCost {cost_mst}\nMuchii\n')
+    for edge in T:
+        fout.write(f'{edge.vertex1} {edge.vertex2}\n')
+    fout.write(f'Al doilea\nCost {cost_secondbestMST}\nMuchii\n')
+    for edge in T:
+        if ((edge.vertex1 == edge_old.vertex1 and edge.vertex2 == edge_old.vertex2) or
+            (edge.vertex1 == edge_old.vertex2 and edge.vertex2 == edge_old.vertex1)):
+            fout.write(f'{edge_new.vertex1} {edge_new.vertex2}\n')
+        else:
+            fout.write(f'{edge.vertex1} {edge.vertex2}\n')
+
 if __name__ == '__main__':
     grafDinamic(WeightedGraph(file=open('grafpond.in', 'r')), WeightedGraphEdge(3, 5, 4), 'Prim')
+    secondBestMST(WeightedGraph(file=open('secondBestMST.in', 'r')), 'Kruskal')
